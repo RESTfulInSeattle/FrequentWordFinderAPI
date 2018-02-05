36d1caa2c46936d1cddc5b236c3bf67d26ff54f2
David Clayton <david@davidclayton.me>
2018-02-04 16:49:50 -0800

    Added DynamoDB Actions

---
+++ FrequentWordFinderAPI/Controllers/DynamoDBItem.cs
@@ -0,0 +1,14 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+
+namespace FrequentWordFinderAPI.Controllers
+{
+    [DynamoDBTable("WordRankerTable")]
+
+    public class DynamoDBItem
+    {
+        [DynamoDBHashKey]
+        public string Id { get; set; }
+        [DynamoDBRangeKey]
+        public string result { get; set; }
+    }
+}
---
+++ FrequentWordFinderAPI/Controllers/DynamoDbActions.cs
@@ -0,0 +1,48 @@
+﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+
+namespace FrequentWordFinderAPI.Controllers
+{
+    public class DynamoDbActions
+    {
+        public string RetrieveItem(string url)
+        {
+            try
+            {
+                var client = new AmazonDynamoDBClient();
+                var context = new DynamoDBContext(client);
+                var item = context.Load<DynamoDBItem>(url, "result");
+
+                return item.result;
+            }
+            catch (AmazonDynamoDBException e)
+            {
+                return "";
+            }
+        }
+
+        public bool AddItem(string url, string result)
+        {
+            try
+            {
+                var client = new AmazonDynamoDBClient();
+                var context = new DynamoDBContext(client);
+                var item = new DynamoDBItem
+                {
+                    Id = url,
+                    result = "Fish"
+                };
+
+                context.Save(item);
+            }
+            catch (AmazonDynamoDBException e)
+            {
+                return false;
+            }
+
+            return true;
+
+
+        }
+    }
+}
